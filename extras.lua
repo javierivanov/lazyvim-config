4b4d7ac47dd21d22beb197853d2a4263c58c688a
Javier Fuentes <j.fuentes.m@icloud.com>
2025-02-10 17:12:32 -0300

    Update extras.lua

--- extras.lua
+++ extras.lua
@@ -12,13 +12,13 @@ return {
 	{ "ellisonleao/gruvbox.nvim" },
 	{ "rebelot/kanagawa.nvim" },
 	{ "Mofiqul/vscode.nvim" },
-	{"catppuccin/nvim"},
-	 -- {
-   -- "LazyVim/LazyVim",
-  --  opts = {
-   --   colorscheme = "catppuccin-mocha",
-  --  },
- -- },
+	{ "catppuccin/nvim" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-mocha",
+		},
+	},
 	{
 		"folke/which-key.nvim",
 		opts = {
@@ -52,5 +52,60 @@ return {
 			"stevearc/dressing.nvim", -- optional for vim.ui.select
 		},
 		config = true,
+	},
+
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+		opts = {
+			-- add any opts here
+			-- for example
+			provider = "gemini",
+			gemini = {
+				model = "gemini-2.0-flash", -- your desired model (or use gpt-4o, etc.)
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
 	},
 }
